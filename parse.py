d4b3ec978942da7cd8aae354c4ef7144a5601ab7
Jake <68929154+jake-87@users.noreply.github.com>
2020-11-10 14:17:15 +1100

    Update parse.py

--- parse.py
+++ parse.py
@@ -3,7 +3,6 @@ def parse(code,i,data):
   print(code[i])
   if code[i] == "mov":
     mov(code,i,data)
-  pass
 datadict = {
   "eax":0,
   "ebx":1,
@@ -16,4 +15,3 @@ datadict = {
 }
 def mov(code,i,data):
   cfg.data[datadict.get(code[i+1])] = code[i+2]
-  
