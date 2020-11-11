50676aae95b5f2c5ce6a74f9239028de02eff69f
J87 <68929154+jake-87@users.noreply.github.com>
2020-11-12 08:26:28 +1100

    Update parse.py

--- parse.py
+++ parse.py
@@ -2,6 +2,8 @@ import cfg
 def parse(code,i,data):
   if code[i] == "mov":
     mov(code,i,data)
+  if code[i] ==  "int":
+    interrupt(code,i,data)
 datadict = {
   "eax":0,
   "ebx":1,
@@ -15,12 +17,17 @@ datadict = {
 def mov(code,i,data):
   cfg.data[datadict.get(code[i+1])] = code[i+2]
 def interrupt(code,i,data):
+  #print("interupt~~")
+  #print(code[i+1])
   if code[i+1] == "0x80" or "128":
+    #print(data[0])
     if str(data[0]) == "4" or 4:
       doprint(code,i,data)
     if str(data[0]) == "1":
       cfg.breakvar = 1
       cfg.exitcode = data[1]
 def doprint(code,i,data):
+  #print(len(data[2])," is len(data[2])")
+  #print(data[3], " is  data[3]")
   if str(len(cfg.data[2])) == str(cfg.data[3]):
     print(cfg.data[2])
