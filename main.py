e1d7a9eda3c192f065e0178c5fe10b73814b1233
J87 <68929154+jake-87@users.noreply.github.com>
2020-11-11 14:01:05 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from parse import parse
-try:
-    import cfg
-    cfg.breakvar = 0
-    cfg.data = [0]*100
-    cfg.exitcode = 0
-except:
-    print("Error: varables cannot be imported. Make sure that  cfg.py exists and is in the same directory as main.py")
-lines = []
-while True:
-    line = input()
-    if line != "EOF":
-        lines.append(line)
-    else:
-        break
-text = ' '.join(lines)
-text=text.replace(","," ")
-code=text.split()
-i=0
-for x in enumerate(code):
-    if cfg.breakvar == 1:
-      break
-    else:
-      parse(code,i,cfg.data)
-    i=i+1
-if cfg.breakvar==0:
-    while True:
-        pass
-print("Exited with code ",cfg.exitcode)
+import cfg
+def parse(code,i,data):
+  if code[i] == "mov":
+    mov(code,i,data)
+  if code[i] ==  "int":
+    interrupt(code,i,data)
+datadict = {
+  "eax":0,
+  "ebx":1,
+  "ecx":2,
+  "edx":3,
+  "var1":4,
+  "var2":5,
+  "var3":6,
+  "var4":7
+}
+def mov(code,i,data):
+  cfg.data[datadict.get(code[i+1])] = code[i+2]
+def interrupt(code,i,data):
+  #print("interupt~~")
+  #print(code[i+1])
+  if code[i+1] == "0x80" or "128":
+    #print(data[0])
+    if str(data[0]) == "4" or 4:
+      doprint(code,i,data)
+    if str(data[0]) == "1":
+      cfg.breakvar = 1
+      cfg.exitcode = data[1]
+def doprint(code,i,data):
+  #print(len(data[2])," is len(data[2])")
+  #print(data[3], " is  data[3]")
+  if str(len(cfg.data[2])) == str(cfg.data[3]):
+    print(cfg.data[2])
